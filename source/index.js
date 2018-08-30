f74fddfa9e58100be60c74c648ef172dae39ade5
L1lith <lily@lillith.pw>
2018-08-30 00:10:10 -0600

    update package version

--- source/index.js
+++ source/index.js
@@ -1,4 +1,4 @@
 const directoryRoutes = require('./directoryRoutes')
-const expressRoutes = require('./expressRoutes')
 
-module.exports = Object.freeze(Object.assign((...args)=>directoryRoutes(...args), {directoryRoutes, expressRoutes}))
+// Allow Future Second Properties
+module.exports = Object.freeze(Object.assign((...args)=>directoryRoutes(...args), {directoryRoutes}))
