750931eea31f87a38b7b5cb1cddc33c128aa8c5d
L1lith <lily@lillith.pw>
2018-10-16 13:20:16 -0600

    add support for dependency injection

--- source/expressRouter.js
+++ source/expressRouter.js
@@ -5,7 +5,23 @@ const {
 
 const validRouteMethods = ["checkout", "copy", "delete", "get", "head", "lock", "merge", "mkactivity", "mkcol", "move", "m-search", "notify", "options", "patch", "post", "purge", "put", "report", "search", "subscribe", "trace", "unlock", "unsubscribe"]
 
-function expressRouter(path, callback) {
+function expressRouter() {
+  if (arguments.length > 3) throw new Error("Too many arguments")
+  if (arguments.length < 1) throw new Error("Missing Path Arguments")
+  let resources = path = callback = null
+  if (arguments.length === 3) {
+    [path, resources, callback] = arguments
+  } else if (arguments.length === 2) {
+    if (typeof arguments[1] == 'object' && arguments[1] !== null) {
+      [path, resources] = arguments
+    } else {
+      [path, callback] = arguments
+    }
+  }
+  if (resources instanceof Promise) resources = await resources
+  if (typeof path != 'string' || path.length < 1) throw new Error("Path Argument must be a String.")
+  if (callback !== null && typeof callback != 'function') throw new Error("Callback Argument Must be a Function or Null.")
+  if (typeof resources != 'object') throw new Error("Resources Argument must be an Object or Null.")
   const result = (async () => {
     const router = new Router()
     const routes = await directoryRoutes(path)
@@ -31,6 +47,7 @@ function expressRouter(path, callback) {
       }
       if (typeof output == 'object' && output !== null) {
         if (!output.hasOwnProperty('handler')) throw `Missing Route Handler Property For Route ${JSON.stringify(path)}`
+        if (output.withResources === true) output.handler = await output.handler(resources)
         if (typeof output.handler != 'function') throw `Invalid Route Handler Property For Route ${JSON.stringify(path)}`
         if (typeof output.middleware == 'function') {
           output.middleware = [output.middleware]
@@ -38,7 +55,7 @@ function expressRouter(path, callback) {
         if (output.hasOwnProperty('method') && (typeof output.method != 'string' || !validRouteMethods.includes(output.method.toLowerCase()))) throw `Invalid Route Method For Route ${JSON.stringify(path)}`
         router[output.method ? output.method.toLowerCase() : 'get']('/' + path, ...output.middleware || [], output.handler)
       } else if (typeof output == 'function') {
-        router.get('/' + path, output)
+        router.get('/' + path, output.handler)
       } else {
         throw `Invalid Route Handler for Route ${JSON.stringify(path)}`
       }
