6a7f567964370e1759b9fab914f93982d0db4ad9
L1lith <lily@lillith.pw>
2018-10-18 23:23:03 -0600

    add support for dependency injection in directory routes

--- source/directoryRoutes.js
+++ source/directoryRoutes.js
@@ -2,7 +2,9 @@ const dirTree = require('directory-tree')
 const {relative} = require('path')
 
 function getDirectoryRoutes(directory, callback) {
-  const result = new Promise((resolve, reject) => {
+  if (typeof directory != 'string' || directory.length < 1) throw new Error("Directory must be a String")
+  if (arguments.length < 1 && callback !== null && typeof callback != 'function') throw new Error("Callback must be a Function")
+  const result = (async () => {
     const output = []
     dirTree(directory, {extensions: /\.js$/}, item => {
       let route = relative(directory, item.path)
@@ -10,8 +12,11 @@ function getDirectoryRoutes(directory, callback) {
       const data = require(item.path)
       output.push([route, data])
     })
-    resolve(output)
-  })
+    for (let i = 0; i < output.length; i++) {
+      const [route, data] = output[i]
+      if (data instanceof Promise) output[i][1] = await data
+    }
+  })()
   if (typeof callback == 'function') {
     result.then(routes => {
       callback(null, routes)
@@ -22,4 +27,29 @@ function getDirectoryRoutes(directory, callback) {
   return result
 }
 
-module.exports = getDirectoryRoutes
+async function getDirectoryRoutesWithResourceSupport(directory) {
+  if (arguments.length < 3) throw new Error("Too Many Arguments")
+  let resources = callback = null
+  if (arguments.length === 3 ) {
+    resources = arguments[1]
+    callback = arguments[2]
+  } else if (typeof arguments[1] == 'object') {
+    resources = arguments[1]
+  } else if (arguments.length === 2) {
+    callback = arguments[1]
+  }
+  if (typeof resources != 'object') throw new Error("Resources must be an Object or Null!")
+  if (callback !== null && typeof callback != 'function') throw new Error("Callback must be a Function or Null")
+  const routes = await directoryRoutes(directory)
+  for (let i = 0; i < routes.length; i++) {
+    const [route, data] = routes[i]
+    if (typeof data == 'object' && data !== null && data.withResources === true) {
+      if (typeof data.handler != 'function') throw new Error(`Resources handler must be a function at path "${route}"`)
+      if (resources === null) throw new Error(`Resources cannot be passed, no resources defined. Error At path "${route}"`)
+      routes[i][1] = await data.handler(resources)
+    }
+  }
+  return routes
+}
+
+module.exports = getDirectoryRoutesWithResourceSupport
