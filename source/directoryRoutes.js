d8176761f45be79f79ce2e4be4162e45f30fad9a
L1lith <lily@lillith.pw>
2018-10-23 15:08:45 -0600

    fix bug with resource handling in directoryRoutes

--- source/directoryRoutes.js
+++ source/directoryRoutes.js
@@ -3,7 +3,7 @@ const parseArgs = require('./parseArgs')
 const {relative} = require('path')
 
 function directoryRoutes() {
-  const {directory, options, callback} = parseArgs(arguments)
+  const {directory, options, callback} = parseArgs([...arguments])
   const result = (async () => {
     const output = []
     dirTree(directory, {extensions: /\.js$/}, item => {
@@ -22,9 +22,10 @@ function directoryRoutes() {
       for (let i = 0; i < output.length; i++) {
         const [route, data] = output[i]
         if (typeof data == 'object' && data !== null && data.hasOwnProperty('withResources')) {
-          if (typeof data.withResources != 'function') throw new Error(`Resources handler must be a function at path "${route}"`)
+          if (typeof data.withResources != 'boolean') throw new Error(`withResources must be a boolean at path "${route}"`)
           if (options.resources === null) throw new Error(`Resources cannot be passed because there are no resources defined. Error At path "${route}"`)
-          output[i][1] = await data.withResources(options.resources)
+          if (typeof data.handler != 'function') throw new Error(`Handler must be exported at path ${route}`)
+          output[i][1] = await data.handler(options.resources)
         }
       }
     }
